9cdab077a8788333699df226b74b92110d223c31
APeev89 <apostol.peev@mail.bg>
2022-06-11 21:22:57 +0300

    Added Builder patter example

---
+++ DesignPatternsCSharp/DesignPatternsCSharp/BuilderPattern/Components/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsCSharp.BuilderPattern.Components
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string BodyStyle { get; set; }
+        public string FuelType { get; set; }
+        public string Color { get; set; }
+        public int Seats { get; set; }
+        public string TransmissionType { get; set; }
+
+        public decimal Price { get; set; }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Id: {Id}");
+            sb.AppendLine($"Make: {Make} Model: {Model}");
+            sb.AppendLine($"Body Style: {BodyStyle}");
+            sb.AppendLine($"Fuel Type: {FuelType}, Transmission Type: {TransmissionType}");
+            sb.AppendLine($"Seats: {Seats} Color: {Color}");
+            sb.AppendLine($"Price: ${Price} ");
+
+            return sb.ToString().TrimEnd(); ;
+        }
+
+    }
+}
